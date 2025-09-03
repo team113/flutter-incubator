168428fc5255697bbc4438c047769ed2e07f8f56
Dipam Patel <61804421+dipam007@users.noreply.github.com>
2025-09-03 15:53:25 +0530

    Update task_1.dart
    I have completed task on extension. Thanks

--- 1_dart_concepts/1_1_mixins/task_1.dart
+++ 1_dart_concepts/1_1_mixins/task_1.dart
@@ -1,4 +1,20 @@
+extension DateTimeX on DateTime {
+  String myFormat() =>
+      "${year.toString().padLeft(4, '0')}"
+      ".${month.toString().padLeft(2, '0')}"
+      ".${day.toString().padLeft(2, '0')} "
+      "${hour.toString().padLeft(2, '0')}:"
+      "${minute.toString().padLeft(2, '0')}:"
+      "${second.toString().padLeft(2, '0')}";
+}
+
 void main() {
   // Implement an extension on [DateTime], returning a [String] in format of
   // `YYYY.MM.DD hh:mm:ss` (e.g. `2023.01.01 00:00:00`).
+
+  final now = DateTime.now();
+  /// Normally DateTime gives
+  print(now.toString()); // Example: 2025-09-03 15:51:25.749801
+  /// Using custome extension DateTime gives
+  print(now.myFormat()); // Example: 2025.09.03 15:51:25
 }
